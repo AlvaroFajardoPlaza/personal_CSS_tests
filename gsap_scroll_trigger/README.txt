7b5d3f1f737596a756d1b08ab34d3d5b546141d8
alvaro <alvaro.fajardoplaz@gmail.com>
2024-01-10 11:02:23 +0100

    añadimos 1º prueba con gsap-scroll trigger corregidos los estilos y aplicadas las transiciones y animaciones al hacer scroll.

---
+++ gsap_scroll_trigger/README.txt
@@ -0,0 +1,5 @@
+Dentro de esta carpeta vamos a explorar la librería GSAP para crear paginas dinaminzadas al hacer scroll.
+
+SCRIPTS >>>>>>>>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js"></script>
---
+++ gsap_scroll_trigger/main.js
@@ -0,0 +1,128 @@
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const sections = gsap.utils.toArray('section');
+
+    // 1º En esta variable scrollTween configuramos un scroll sobre todo el wrapper!
+    let scrollTween = gsap.to(sections, {
+        xPercent: -100 * (sections.length - 1),
+        ease: 'none',
+        scrollTrigger: {
+            trigger: '.wrapper',
+            pin: true, 
+            scrub: 0.5,
+            snap: 1 / (sections.length - 1),
+            start: 'top top',
+            end: 3000,
+        }
+    })
+
+    // Animación de sroll sobre el logo
+    gsap.to('.logo', {
+        fontSize: '3rem',
+        lineHeight: '2.6rem',
+        top: '4rem',
+        scrollTrigger: {
+            trigger: '.logo',
+            start: 'top top',
+            end: 1500,
+            scrub: 0.5,
+        }
+    })
+
+    // Creamos animación sobre las líneas de la primera sección
+    gsap.to('.line', {
+        height: '66rem',
+        scrollTrigger: {
+            trigger: '.line',
+            scrub: 0.5,
+            start: 'center center',
+            end: 2000
+        }
+    })
+
+    // 2º Creamos animación para los personajes mediante un querySelectorAll
+    document.querySelectorAll('.character').forEach( (elem) => {
+        
+        // Animación de la caption
+        gsap.to(elem.querySelector('.caption'), {
+            x: 0,
+            y: 0, 
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: elem.querySelector('.caption'),
+                start: 'top bottom',
+                end: '+=1000',
+                scrub: 0.5
+            }
+        });
+
+        // Animación de la quote
+        gsap.to(elem.querySelector('.quote'), {
+            y: 0, 
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: elem.querySelector('.quote'),
+                start: 'top bottom',
+                end: '+=30%',
+                scrub: 0.5,
+            }
+        })
+
+        // Idem para el slogan
+        gsap.to(elem.querySelector('.slogan'), {
+            y: 0, 
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: elem.querySelector('.slogan'),
+                start: 'top bottom',
+                end: '+=60%',
+                scrub: 0.5,
+            }
+        })
+
+        // Idem para el block y la imagen!
+        gsap.to(elem.querySelector('.block'), {
+            x: 0, 
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: elem.querySelector('.block'),
+                start: 'top bottom',
+                end: '+=' + window.innerWidth,
+                scrub: 0.5,
+            }
+        })
+
+        gsap.to(elem.querySelector('img'), {
+            y: 0, 
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: elem.querySelector('img'),
+                start: 'top bottom',
+                end: '+=60%',
+                scrub: 0.5,
+            }
+        })
+
+
+        // Finalmente, vamos a modificar el huge-text!
+        gsap.to(elem.querySelector('.huge_text'), {
+            y: 0,
+            ease: 'none',
+            scrollTrigger: {
+                containerAnimation: scrollTween,
+                trigger: elem.querySelector('.huge_text'),
+                start: 'top bottom',
+                end: '+=60%',
+                scrub: 0.5,
+            }
+        })
+
+
+    })
+})
